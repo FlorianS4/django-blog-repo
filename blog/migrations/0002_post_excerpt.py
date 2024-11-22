1beb525c4f7726a8b58092a6809740c3c5167e32
Florian Sassmann <florian.sassmann@hotmail.com>
2024-11-22 11:17:00 +0000

    complete Post databank with data and additional fields

--- blog/migrations/0002_post_excerpt.py
+++ blog/migrations/0002_post_excerpt.py
@@ -4,20 +4,13 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('blog', '0001_initial'),
     ]
-
     operations = [
         migrations.AddField(
             model_name='post',
             name='excerpt',
             field=models.TextField(blank=True),
-        ),
-        migrations.AddField(
-            model_name='post',
-            name='updated_on',
-            field=models.DateTimeField(auto_now=True),
         ),
     ]
